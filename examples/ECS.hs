b15b541e78ce9559d898d60e78a07f505a123965
Matt Hunzinger <matthunz2@gmail.com>
2025-01-12 14:38:59 -0500

    feat: setup typed archetypes

--- examples/ECS.hs
+++ examples/ECS.hs
@@ -3,33 +3,11 @@
 
 module Main where
 
-import Control.Monad.IO.Class
-import Data.Aztecs.Edit (Edit)
-import qualified Data.Aztecs.Edit as C
-import Data.Aztecs.Entity
-import qualified Data.Aztecs.Query as Q
-import qualified Data.Aztecs.World as W
+import Data.Aztecs
 import Text.Pretty.Simple
-
-newtype Position = Position Int deriving (Show)
-
-instance Component Position
-
-newtype Velocity = Velocity Int deriving (Show)
-
-instance Component Velocity
-
-app :: Edit IO ()
-app = do
-  C.spawn_ $ entity (Position 0) <&> Velocity 1
-  C.spawn_ $ entity (Position 2) <&> Velocity 2
-
-  positions <- Q.map $
-    \(Position p :& Velocity v) -> Position (p + v)
-
-  liftIO $ pPrint positions
 
 main :: IO ()
 main = do
-  _ <- C.runEdit app W.empty
-  return ()
+  let a = arch @Int [1, 2, 3]
+      b = query @_ @'[Int] a
+  pPrint b
--- src/Data/Aztecs.hs
+++ src/Data/Aztecs.hs
@@ -1,1 +1,61 @@
-module Data.Aztecs () where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Data.Aztecs where
+
+import Data.Kind (Type)
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+
+data Entity (ts :: [Type]) where
+  ENil :: Entity '[]
+  ECons :: t -> Entity ts -> Entity (t ': ts)
+
+instance Show (Entity '[]) where
+  show ENil = "[]"
+
+instance (Show t, ShowEntity (Entity ts)) => Show (Entity (t ': ts)) where
+  show (ECons x xs) = "[" ++ show x ++ showEntity xs
+
+class ShowEntity a where
+  showEntity :: a -> String
+
+instance ShowEntity (Entity '[]) where
+  showEntity ENil = "]"
+
+instance (Show t, ShowEntity (Entity ts)) => ShowEntity (Entity (t ': ts)) where
+  showEntity (ECons x xs) = ", " ++ show x ++ showEntity xs
+
+data Archetype (ts :: [Type]) where
+  ANil :: Archetype '[]
+  ACons :: Vector t -> Archetype ts -> Archetype (t ': ts)
+
+arch :: [a] -> Archetype '[a]
+arch as = ACons (V.fromList as) ANil
+
+class Match (as :: [Type]) (bs :: [Type]) where
+  match :: Archetype as -> [Entity bs]
+
+instance Match '[] '[] where
+  match ANil = []
+
+instance {-# OVERLAPS #-} Match '[a] '[a] where
+  match (ACons v _) =
+    let bs = (V.toList v)
+        f a = ECons a ENil
+     in map f bs
+
+instance (Match as bs) => Match (a ': as) (a ': bs) where
+  match (ACons v as) =
+    let bs = (V.toList v)
+        es = match as
+        f (a, b) = ECons a b
+     in map f (zip bs es)
+
+query :: (Match as bs) => Archetype as -> [Entity bs]
+query = match
