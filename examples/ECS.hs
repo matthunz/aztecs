f38cba656e05cf3a91ff9389afb68e9c90324ef4
Matt Hunzinger <matthunz2@gmail.com>
2025-01-13 02:44:38 -0500

    feat: add lookup fn

--- examples/ECS.hs
+++ examples/ECS.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import Data.Aztecs.Archetype
-import Text.Pretty.Simple
+import qualified Data.Aztecs.Archetype as A
 import Data.Aztecs.Entity (Entity (ECons, ENil))
+import Text.Pretty.Simple
 
 main :: IO ()
 main = do
   let x = ACons (pure (1 :: Int)) (ACons (pure True) ANil)
-      y = Data.Aztecs.Archetype.map  x  (\((ECons i ENil) :: Entity '[Int]) -> ECons (i + 1) ENil)
-  pPrint y
+      y = Data.Aztecs.Archetype.map x (\((ECons i ENil) :: Entity '[Int]) -> ECons (i + 1) ENil)
+      z = A.lookup @'[Bool] 0 x
+  pPrint z
--- src/Data/Aztecs/Archetype.hs
+++ src/Data/Aztecs/Archetype.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Data.Aztecs.Archetype where
 
@@ -13,6 +14,7 @@ import Data.Aztecs.Entity
 import Data.Kind (Type)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
+import Prelude hiding (lookup)
 
 data Archetype (as :: [Type]) where
   ANil :: Archetype '[]
@@ -36,10 +38,12 @@ instance (Show a, ShowArchetype (Archetype as)) => ShowArchetype (Archetype (a '
 class Match (as :: [Type]) (es :: [Type]) where
   match :: Archetype as -> [Entity es]
   alter :: [Entity es] -> Archetype as -> Archetype as
+  lookup' :: Int -> Archetype as -> Maybe (Entity es)
 
 instance Match as '[] where
   match _ = []
   alter _ as = as
+  lookup' _ _ = Nothing
 
 instance {-# OVERLAPS #-} (Has (Vector e) (Archetype as)) => Match as '[e] where
   match as =
@@ -50,17 +54,24 @@ instance {-# OVERLAPS #-} (Has (Vector e) (Archetype as)) => Match as '[e] where
     let (es', es'') = unzip $ fmap (\(ECons e t) -> (e, t)) es
         as' = setComponent (V.fromList $ es') as
      in alter es'' as'
+  lookup' i as = do
+    e <- component as V.!? i
+    return $ ECons e ENil
 
-instance (Match as es, Has [e] (Archetype as)) => Match as (e ': es) where
+instance (Match as es, Has (Vector e) (Archetype as)) => Match as (e ': es) where
   match as =
-    let es = component as
+    let es = V.toList $ component as
         entities = match as
         f (e, y) = ECons e y
      in fmap f (zip es entities)
   alter es as =
     let (es', es'') = unzip $ fmap (\(ECons e t) -> (e, t)) es
-        as' = setComponent es' as
+        as' = setComponent (V.fromList es') as
      in alter es'' as'
+  lookup' i as = do
+    e <- component as V.!? i
+    es <- lookup' i as
+    return $ ECons e es
 
 instance {-# OVERLAPPING #-} Has (Vector a) (Archetype (a ': ts)) where
   component (ACons v _) = v
@@ -75,3 +86,8 @@ map a f =
   let es = match a
       es' = fmap f es
    in alter es' a
+
+lookup :: forall  (es :: [Type]) (as :: [Type]).
+  Match as es =>
+  Int -> Archetype as -> Maybe (Entity es)
+lookup = lookup'
