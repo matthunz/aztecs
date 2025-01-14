fbef09d779323abb599c49d84caafcd963e1a386
Matt Hunzinger <matthunz2@gmail.com>
2025-01-14 17:50:12 -0500

    feat: setup new Entity and Archetype types

--- src/Data/Aztecs.hs
+++ src/Data/Aztecs.hs
@@ -1,5 +1,25 @@
-module Data.Aztecs (EntityID(..)) where
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
--- | EntityIDID.
-newtype EntityID= EntityID{unEntityID:: Int}
+module Data.Aztecs (EntityID (..)) where
+
+import Control.Monad.Identity (Identity)
+import Data.Kind (Type)
+import Data.Vector (Vector)
+
+newtype EntityID = EntityID {unEntityID :: Int}
   deriving (Eq, Ord, Show)
+
+data Row f (as :: [Type]) where
+  Nil :: Row f '[]
+  Cons :: f a -> Row f as -> Row f (a ': as)
+
+newtype Entity as = Entity {unEntity :: Row Identity as}
+
+newtype Archetype as = Archetype {unArchetype :: Row Vector as}
