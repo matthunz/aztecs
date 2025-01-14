26907c9f23ce0c71eeecc91626017c0bead98f9e
Matt Hunzinger <matthunz2@gmail.com>
2025-01-14 16:07:45 -0500

    refactor!: start rewrite

--- examples/ECS.hs
+++ examples/ECS.hs
@@ -3,33 +3,6 @@
 
 module Main where
 
-import Control.Monad.IO.Class
-import Data.Aztecs.Edit (Edit)
-import qualified Data.Aztecs.Edit as C
-import Data.Aztecs.Entity
-import qualified Data.Aztecs.Query as Q
-import qualified Data.Aztecs.World as W
-import Text.Pretty.Simple
-
-newtype Position = Position Int deriving (Show)
-
-instance Component Position
-
-newtype Velocity = Velocity Int deriving (Show)
-
-instance Component Velocity
-
-app :: Edit IO ()
-app = do
-  C.spawn_ $ entity (Position 0) <&> Velocity 1
-  C.spawn_ $ entity (Position 2) <&> Velocity 2
-
-  positions <- Q.map $
-    \(Position p :& Velocity v) -> Position (p + v)
-
-  liftIO $ pPrint positions
-
 main :: IO ()
 main = do
-  _ <- C.runEdit app W.empty
   return ()
